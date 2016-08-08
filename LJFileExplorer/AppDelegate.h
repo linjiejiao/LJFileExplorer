f9e1fc14a2afdf506b0c6a0f5c44b948cb4439f1
linjiejiao <hpckljj@gmail.com>
2016-08-08 17:37:41 +0800

    first commit

---
+++ LJFileExplorer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJFileExplorer
+//
+//  Created by liangjiajian_mac on 16/8/8.
+//  Copyright © 2016年 cn.ljj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJFileExplorer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJFileExplorer
+//
+//  Created by liangjiajian_mac on 16/8/8.
+//  Copyright © 2016年 cn.ljj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
